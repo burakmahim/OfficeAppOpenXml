32d55b39020acf422632636962e83fb5cff12528
saliha <salihaunersoy@outlook.com>
2025-08-19 10:21:39 +0300

    commit

--- OfficeAppOpenXml/PowerPointLibrary.cs
+++ OfficeAppOpenXml/PowerPointLibrary.cs
@@ -1,14 +1,29 @@
-﻿using System;
+﻿using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
+using DocumentFormat.OpenXml.Presentation;
+using OfficeAppOpenXmlLibrary.PowerPointOpenXmlComponents;
 
 namespace OfficeAppOpenXmlLibrary
 {
     public class PowerPointLibrary
     {
+        public void CreatePowerPointPresentation()
+        {
+            using (MemoryStream memoryStream = new MemoryStream())
+            {
+                using (PresentationDocument presentationDoc = PresentationDocument.Create(memoryStream, PresentationDocumentType.Presentation, true))
+                {
+                    PresentationPart presentationPart = presentationDoc.AddPresentationPart();
+                    presentationPart.Presentation = new Presentation();
 
-        
-              //aaaaa
+                    SlideMasterPart slideMasterPart = presentationPart.AddNewPart<SlideMasterPart>();
 
+                }
+            }
+        }
     }
 }
